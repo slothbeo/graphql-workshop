366df94f8f7af2375e846292479ae43dc5f3451a
mkol5222 <mkoldov@gmail.com>
2023-11-15 11:36:06 +0000

    .

--- 01-simple-server/resolvers.ts
+++ 01-simple-server/resolvers.ts
@@ -13,6 +13,15 @@ function sum2(_: any, { a, b }: { a: number, b: number }) {
   return a+b;
 }
 
+
+function multiply(_: any, args) {
+  console.log('multiply', JSON.stringify(args, null, 2))
+  return { 
+    num: (args.x * args.y),
+    greeting: "Hi from server!"
+  };
+}
+
 function minus(_: any, args: { a: number, b: number }) {
   return args.a - args.b;
 }
@@ -23,7 +32,8 @@ const resolvers = {
     sum2,
     helloWorld,
     hello,
-    minus
+    minus,
+    multiply
   },
 };
 
